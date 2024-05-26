c0c33d03aa93f01aef09e39bff508e5ea9ebe477
Megagig <megagigsoftwaresolutions.com>
2024-05-26 23:41:59 +0100

    Base Setup of the Application structure

---
+++ src/pages/MovieList.jsx
@@ -0,0 +1,7 @@
+export const MovieList = () => {
+  return (
+    <main>
+      <h1>Movie List</h1>
+    </main>
+  );
+};
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,7 @@
+export const PageNotFound = () => {
+  return (
+    <main>
+      <h1>Page Not Found</h1>
+    </main>
+  );
+};
