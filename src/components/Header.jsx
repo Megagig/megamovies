c30b6d9673db1fad41f6b6e9569b9e96495e7d08
Megagig <megagigsoftwaresolutions.com>
2024-05-29 07:08:38 +0100

    Implement active and inActive class on the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,10 @@ import { Link, NavLink } from 'react-router-dom';
 import Logo from '../assets/Logo.jpeg';
 
 export const Header = () => {
+  const activeClass =
+    'text-base block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500';
+  const inactiveClass =
+    'text-base block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700';
   return (
     <header>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -123,7 +127,9 @@ export const Header = () => {
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <NavLink
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                  className={({ isActive }) =>
+                    isActive ? activeClass : inactiveClass
+                  }
                   end
                   to="/"
                 >
@@ -132,7 +138,9 @@ export const Header = () => {
               </li>
               <li>
                 <NavLink
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className={({ isActive }) =>
+                    isActive ? activeClass : inactiveClass
+                  }
                   to="/movies/popular"
                 >
                   Popular
@@ -140,7 +148,9 @@ export const Header = () => {
               </li>
               <li>
                 <NavLink
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className={({ isActive }) =>
+                    isActive ? activeClass : inactiveClass
+                  }
                   to="/movies/top"
                 >
                   Top rated
@@ -148,7 +158,9 @@ export const Header = () => {
               </li>
               <li>
                 <NavLink
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className={({ isActive }) =>
+                    isActive ? activeClass : inactiveClass
+                  }
                   to="/movies/upcoming"
                 >
                   Upcoming
