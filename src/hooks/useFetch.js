87cc844f84c1d3e73a3358548b44c1abdc65d274
Megagig <megagigsoftwaresolutions.com>
2024-05-31 09:50:49 +0100

    individuality for each Route-Defining API Path for each Route

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 
-export const useFetch = (url) => {
+export const useFetch = (apiPath) => {
   const [data, setData] = useState([]);
+  const url = ` https://api.themoviedb.org/3/${apiPath}?api_key=c012f0436866bbf5f024bee3107b5b7d`;
 
   useEffect(() => {
     const fetchMovies = async () => {
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,10 +1,8 @@
 import { useFetch } from '../hooks/useFetch';
 import { Card } from '../components/Card';
 
-export const MovieList = () => {
-  const { data: movies } = useFetch(
-    'https://api.themoviedb.org/3/movie/now_playing?api_key=c012f0436866bbf5f024bee3107b5b7d'
-  );
+export const MovieList = ({ apiPath }) => {
+  const { data: movies } = useFetch(apiPath);
 
   return (
     <main>
