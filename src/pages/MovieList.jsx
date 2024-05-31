b79257d12b6a66a04344cb00aa04b495057ef6d5
Megagig <megagigsoftwaresolutions.com>
2024-05-31 07:46:32 +0100

    Finish working on API Fetch movies list

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,17 +1,28 @@
+import { useState, useEffect } from 'react';
 import { Card } from '../components/Card';
 
 export const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+
+  //Fetch movies using the useEffect hook on the first run
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=c012f0436866bbf5f024bee3107b5b7d'
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    };
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap">
-          {/* inside this div, we are going to have a loop of multiple moves */}
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
         </div>
       </section>
     </main>
