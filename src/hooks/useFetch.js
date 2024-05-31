aa28231190be95ccbce69e82960a28bd261f6628
Megagig <megagigsoftwaresolutions.com>
2024-05-31 10:36:05 +0100

    Hide The API Key using the env files in a vite application

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,7 +2,9 @@ import { useState, useEffect } from 'react';
 
 export const useFetch = (apiPath) => {
   const [data, setData] = useState([]);
-  const url = ` https://api.themoviedb.org/3/${apiPath}?api_key=c012f0436866bbf5f024bee3107b5b7d`;
+  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${
+    import.meta.env.VITE_APP_API_KEY
+  }`; // import.meta.env.VITE_APP_API_KEY
 
   useEffect(() => {
     const fetchMovies = async () => {
