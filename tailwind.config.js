7ae96d6ed6c60fddf000fe76276d30837389221b
Megagig <megagigsoftwaresolutions.com>
2024-05-31 15:14:49 +0100

    Tailwind CSS Customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        other: { min: '340px', max: '1200px' },
+      },
+      colors: {
+        darkbg: '#1E293B',
+      },
+    },
   },
   plugins: [],
 };
